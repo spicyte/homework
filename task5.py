1a043333c1ee27f53203bfcdb0d5716e520f58b5
spicyte <115945041+spicyte@users.noreply.github.com>
2023-10-30 06:25:00 +0200

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,4 @@
+name = "Kate"
+surname = "Kovalenko"
+name_and_surname = name + " " + surname
+print(f"Hello, {name_and_surname}")
---
+++ task6.py
@@ -0,0 +1,9 @@
+a = 100
+b = 15
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a//b)
