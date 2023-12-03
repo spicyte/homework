cb3ed6943f4866af2eb9cef3938e996bf418ee5a
Kate Kovalenko <115945041+spicyte@users.noreply.github.com>
2023-12-03 20:58:52 +0200

    Update 16.1.py

--- 16.1.py
+++ 16.1.py
@@ -8,7 +8,7 @@ class Student(Person):
         print("student method")
         return c
 
-# Creating an instance of Student
+
 student_instance = Student()
 print(student_instance.method(name="Kate"))
 
@@ -18,7 +18,6 @@ class Teacher(Person):
         print("teacher method")
         return a
 
-# Creating an instance of Teacher
 teacher_instance = Teacher()
 print(teacher_instance.method(name="Anna", salary=8000))
 
