7fffedf64ddddbcc7d1fef1c3b004a05ae0dd964
Kate Kovalenko <115945041+spicyte@users.noreply.github.com>
2023-12-22 20:17:05 +0200

    Update 20,1.py

--- 20,1.py
+++ 20,1.py
@@ -6,7 +6,7 @@ def add_number(a, b):
 class TestAddNumber(unittest.TestCase):
     def test_add_number(self):
         self.assertEqual(add_number(10, 5), 15)
-        self.assertEqual(add_number(-10, -5), -15)  # Исправлено знак числа
+        self.assertEqual(add_number(-10, -5), -15)
         self.assertEqual(add_number(-10, 5), -5)
 
 if __name__ == '__main__':
